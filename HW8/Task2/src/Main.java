a5cae945ef9d02bd83d1898b3c40f0fb700c78f4
monblank <monblank@localhost>
2023-01-15 23:47:20 +0500

    HW Done

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+    	int[] array = new int[10];
+    	int min = 40;
+    	int max = 100;
+    	int diff = max - min;
+    	Random random = new Random();
+    	int sum = 0;
+    	int count = 0;
+    	for (int i = 0; i < array.length; i++) {
+    		array[i] = random.nextInt(diff) + min;
+    		System.out.println(array[i]);
+    		sum += array[i];
+    		if (array[i] >= 60 && array[i] <= 80) {
+    			count++;
+    		}
+    	}
+    	System.out.println("Средний вес: " + sum / array.length);
+    	System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно: " + count);
     }
 }
