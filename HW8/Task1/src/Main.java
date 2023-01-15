06cb8fc139ba46cae36178d45aec38b4cbc18d05
monblank <monblank@localhost>
2023-01-15 23:32:59 +0500

    HW Done

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+    	int[] array = new int[10];
+    	int[] arrayTemp = new int[10];
+    	for (int i = 0; i < array.length; i++) {
+    		array[i] = new Random().nextInt(100);
+    		System.out.println(array[i]);
+    	}
+    	for (int i = 0; i < array.length; i++) {
+    		System.out.println(array[array.length - (i + 1)]);
+    	}
+    	for (int i = 0; i < array.length; i++) {
+    		arrayTemp[i] = array[array.length - (i + 1)];
+    		System.out.println(arrayTemp[i]);
+    	}
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
 
